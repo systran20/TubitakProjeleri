f1079ed4df7d85b3e02d7e0d0e18c8e7baef9811
Systran <47870746+systran20@users.noreply.github.com>
2020-11-13 11:54:40 +0300

    Update SerialPrintString.ino

--- CokluArduinoSeriportOku/SerialPrintString.ino
+++ CokluArduinoSeriportOku/SerialPrintString.ino
@@ -1,8 +1,11 @@
+//herbir arduino içinde aşağıdaki basit kod bulunur.
+//farklı farklı işleri yapan ardunolar için değişik veriler seri porta yazdırılır.
+
 void setup() {    
   Serial.begin(9600);
 }
 
 void loop() {   
-  Serial.println("Sicaklik Devresi");
+  Serial.println("Mesajı Buraya Yaz"); //Her arduinonun farklı mesaj göndermesi gerekir.
   delay(1250);
 }
